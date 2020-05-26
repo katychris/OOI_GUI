55bd6fa4d42d417d541a40d65aea5ab80b561e06
h-glover <64487064+h-glover@users.noreply.github.com>
2020-05-26 12:26:53 -0700

    data loading updates 5/26

--- OOI_GUI_DataLoader.py
+++ OOI_GUI_DataLoader.py
@@ -1,9 +1,7 @@
 """
 This code:
 1. loads the downloaded netcdf data into a nice pandas dataframe.
-2. creates a new output folder if one does not exist
 3. saves the pandas dataframe as a pickle file in that folder
-4. Maybe there should be a use input for the file and folder names?
 It should probably get merged with the DataGrabber later on (once it's done).
 HEG (5/25/2020)
 """
@@ -12,33 +10,68 @@ HEG (5/25/2020)
 # imports
 import numpy as np
 import netCDF4 as nc 
+import pandas as pd
 import sys, os
 import ooi_mod # module with the directory creation function
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
-# # get the current working directory:
-# dir_path = pwd
+get the current working directory then only keep name up 2 levels
+# this will be used as the root for /code, /ooi_data, /ooi_output:
+dir_path = os.getcwd()
+a = dir_path.split('/') # makes a list out of the path name
+del a[-2:]# remove last two directories
+dir_path = '/'.join(a)
 
-# # make an output directory for the data, one level up
-# a = dir_path.split('/') # makes a list out of the path name
-# a[-1] = 'ooi_data' # change final directory to be the output data directory
-# out_dir = '/'.join(a) # remerge name
+# make an input data directory:
+in_dir = dir_path + '/ooi_data'
+ooi_mod.make_dir(in_dir)
 
-# # make the output directory, if it does not exist
-# ooi_mod.make_dir(out_dir)
+# make an output directory for the data
+out_dir = dir_path + 'ooi_output'
+ooi_mod.make_dir(out_dir)
 
-# # load the netcdf data:
-# fname = out_dir + selected_datasets
-# ds = nc.Dataset(fnames)
-
-# # save as a pickle file:
-# out_y = out_dir + '.p'
-# pickle.dump(y, open(out_y, 'wb')) # 'wb' is for write binary
+# load the netcdf data:
+ds = nc.MFDataset(in_dir + selected_datasets)
 
 fname = '../../../output/ocn506/test.nc'
 ds=nc.Dataset(fname)
 
-x = ds['lon'][:]
-if x.max() > 180:
-    x = x - 360 # convert to -180:180 format
-y = ds['lat'][:]
+# relevant fields from netcdf file
+flds = ['time','pressure','practical_salinity','temp','density']
+units = []
+for jj in range (0,len(flds)):
+    units.append(ds[flds[jj]].units)
+
+# fix the time stamp:
+t_ooi = ds[flds[0]][:] # pull out the time from the netcdf
+if '1900' in units[0]: # there are two options for the reference year for the OOI time: 1900,1970
+    t0=datetime.toordinal(date(1900,1,1))
+elif '1970' in units[0]:
+    t0=datetime.toordinal(date(1970,1,1))
+
+# use this function to convert to a python datetime
+tt =[]
+for jj in range(0,len(t_ooi)):
+    tt.append(ooi_mod.ooi_to_datetime(t_ooi[jj],t0))
+
+
+# put the data into a pandas data frame for convenient storage
+df = pd.DataFrame(data=tt,columns=[flds[0]])
+for jj in range (1,len(flds)):
+     df.insert(jj,flds[jj],ds[flds[jj]][:])
+
+# set the time (first col) as index:
+df.set_index('time',inplace=True)
+
+# replace fill values with nans:
+df[df==-9999999.0]=np.nan
+
+# get a name for the output pickle file:
+fname = selected_datasets[0]
+fname = fname.split('.') # makes a list out of the path name
+save_name = out_dir + fname[0] + '.p'
+# save the dataframe as a pickle file
+df.to_pickle(save_name)
+# save the metadata as a pickle file:
+pickle.dump(units, open('meta_'+save_name, 'wb')) # 'wb' is for write binary
+
--- ooi_mod.py
+++ ooi_mod.py
@@ -5,6 +5,7 @@ HEG (5/25/2020)
 
 # imports
 import os, sys, shutil
+from datetime import timedelta, datetime
 
 def make_dir(dirname, clean=False):
     """
@@ -19,4 +20,27 @@ def make_dir(dirname, clean=False):
             os.mkdir(dirname)
         except OSError:
             pass # assume OSError was raised because directory already exists
-            
+
+# fix the OOI time stamp and convert to python
+def ooi_to_datetime(datenum,t0):
+    # inputs:
+    #   datenum = an OOI time value
+    #   t0= reference date (from the netcdf units)
+    # OOI time is in seconds from either 1900 or 1970, depending on staiton
+    # python ordinal time starts starts
+
+    # day = integer of datenum (no decimal) - 366
+    DD = t0 + datenum/[24*60*60]
+    DD=datetime.fromordinal(int(DD))
+    # calculate hour, min, sec from the fraction of a day in datenum:
+    HH = datenum % 1 * 24
+    MM = HH % 1 * 60
+    SS = MM % 1 * 60
+    FF = SS % 1 * 1000
+
+    HH = timedelta(hours=int(HH))
+    MM = timedelta(minutes=int(MM))
+    SS = timedelta(seconds=int(SS))
+    FF = timedelta(milliseconds=int(round(FF)))
+    
+    return DD + HH + MM + SS + FF
