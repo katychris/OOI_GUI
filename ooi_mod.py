57bed69e87f798969c52de7bf15351a83b5a1831
katychris <64493615+katychris@users.noreply.github.com>
2020-05-26 18:22:51 -0700

    Update ooi_mod.py

--- ooi_mod.py
+++ ooi_mod.py
@@ -4,7 +4,9 @@ HEG (5/25/2020)
 """
 
 # imports
-import os, sys, shutil
+import os, sys, shutil, re
+import requests
+
 
 def make_dir(dirname, clean=False):
     """
@@ -19,4 +21,45 @@ def make_dir(dirname, clean=False):
             os.mkdir(dirname)
         except OSError:
             pass # assume OSError was raised because directory already exists
-            
+
+
+def get_data(url):
+    '''Function to grab all data from specified THREDDS server'''
+    tds_url = 'https://opendap.oceanobservatories.org/thredds/dodsC'
+    datasets = requests.get(url).text
+    urls = re.findall(r'href=[\'"]?([^\'" >]+)', datasets)
+    x = re.findall(r'(ooi/.*?.nc)', datasets)
+    for i in x:
+        if i.endswith('.nc') == False:
+            x.remove(i)
+    for i in x:
+        try:
+            float(i[-4])
+        except:
+            x.remove(i)
+    datasets = [os.path.join(tds_url, i) for i in x]
+    # Remove extraneous data files if necessary
+    # This makes sure that the stream and the file match
+    catalog_rms = url.split('/')[-2][20:]
+    selected_datasets = []
+    for d in datasets:
+        if catalog_rms == d.split('/')[-1].split('_20')[0][15:]:
+            selected_datasets.append(d + '#fillmismatch') #fillmismatch to deal with a bug
+    selected_datasets = sorted(selected_datasets)
+    return selected_datasets
+
+
+def list_picker(title,data_list,default_val=1):
+    '''Formatting for user input on an item in a list, returns the index of the desired item'''
+
+    print('\n\033[1m' + title + '\033[0m')
+    for ii in range(len(data_list)):
+        print(str(ii+1) + ': ' + data_list[ii])
+
+    my_choice = input('\n Input Selection Number (return='+str(default_val)+'): ')
+    if len(my_choice) == 0:
+        my_choice = str(default_val)
+    print('\n'+ data_list[int(my_choice)-1])
+
+    return my_choice
+
