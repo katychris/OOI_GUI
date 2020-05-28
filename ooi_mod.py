72d1ad6925f005a7d488def997c33a65d003f9d1
katychris <64493615+katychris@users.noreply.github.com>
2020-05-27 17:49:30 -0700

    Update ooi_mod.py

--- ooi_mod.py
+++ ooi_mod.py
@@ -64,3 +64,27 @@ def list_picker(title,data_list,default_val=1):
 
     return my_choice
 
+# fix the OOI time stamp and convert to python
+def ooi_to_datetime(datenum,t0):
+    # inputs:
+    #   datenum = an OOI time value
+    #   t0= reference date (from the netcdf units)
+    # OOI time is in seconds from either 1900 or 1970, depending on staiton
+    # python ordinal time starts starts
+
+    # day = integer of datenum (no decimal) - 366
+    DD = t0 + datenum/[24*60*60]
+    DD=datetime.fromordinal(int(DD))
+    # calculate hour, min, sec from the fraction of a day in datenum:
+    HH = datenum % 1 * 24
+    MM = HH % 1 * 60
+    SS = MM % 1 * 60
+    FF = SS % 1 * 1000
+
+    HH = timedelta(hours=int(HH))
+    MM = timedelta(minutes=int(MM))
+    SS = timedelta(seconds=int(SS))
+    FF = timedelta(milliseconds=int(round(FF)))
+    
+    return DD + HH + MM + SS + FF
+
