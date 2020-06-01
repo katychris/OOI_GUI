2a7aa200bb205f568b7fbef3dab42143dd62992e
Theresa Whorley <twhorley@uw.edu>
2020-06-01 10:49:31 -0700

    update Plotter to run on its own

--- OOI_GUI_DataPlotter.py
+++ OOI_GUI_DataPlotter.py
@@ -2,7 +2,7 @@
 This code plots a measured CTD parameter (T, S, P, rho) in depth v time space.
 Meant to be run immediately after 'OOI_GUI_DataLoader.py' script
 Dependencies: sys, os, numpy, pandas, matplotlib.pyplot, cmocean
-TLW - 5/29/2020
+TLW - 06/01/2020
 """
 
 # Imports
@@ -29,14 +29,21 @@ else:
     sys.exit()
 import matplotlib.pyplot as plt
 
+# get the current working directory then only keep name up 2 levels
+# this will be used as the root for /code, /ooi_data, /ooi_output:
+dir_path = os.getcwd()
+a = dir_path.split('/') # makes a list out of the path name
+dir_name = a[-1]
+del a[-1:]# remove last two directories
+dir_path = '/'.join(a)
 
-# ONLY FOR TESTING  -- read in test pickle file
-#data_dir = '/Users/theresawhorley/effcomp/OOI_GUI_data/'
-#fname = data_dir + 'testfortheresa.p'
-#df = pd.read_pickle(fname)
+# make an output data directory:
+out_dir = dir_path+'/'+dir_name+'_output'
+ooi_mod.make_dir(out_dir)
 
 # read in pickle file
-df.read_pickle(save_name)
+fname1 = input("Station File Name")
+df.read_pickle(fname1)
 
 # make a figure object with axes of pressure v time
 # time is stored as index of DataFrame df
@@ -55,11 +62,11 @@ cm_rho = cmocean.cm.dense  # assign a colormap to density
 t_utc = 'Time (UTC)'  #assign variable to time label
 
 # plot temperature as a function of pressure and time
-sc1 = ax1.scatter(x, y, c=temp, vmin=0, vmax=35, cmap=cm_temp)
+sc1 = ax1.scatter(x, y, c=temp, vmin=0, vmax=20, cmap=cm_temp)
 ax1.tick_params(axis='x', direction='in', labelrotation=70)
 ax1.tick_params(axis='y', direction='in')
 ax1.invert_yaxis()
-ax1.set_title('Tempterature (C)')
+ax1.set_title('Temperature $(C)$')
 ax1.set_xlabel(t_utc)
 ax1.set_ylabel('Pressure (dbar)')
 cbar1 = fig.colorbar(sc1, ax=ax1)
@@ -69,23 +76,23 @@ sc2 = ax2.scatter(x, y, c=ps, vmin=30, vmax=35, cmap=cm_ps)
 ax2.tick_params(axis='x', direction='in', labelrotation=70)
 ax2.tick_params(axis='y', direction='in')
 ax2.invert_yaxis()
-ax2.set_title('Salinity (PSU)')
+ax2.set_title('Salinity $(PSU)$')
 ax2.set_xlabel(t_utc)
 cbar2 = fig.colorbar(sc2, ax=ax2)
 
 # plot density as a function of pressure and time
-sc3 = ax3.scatter(x, y, c=rho, vmin=1024, vmax=1030, cmap=cm_rho)
+sc3 = ax3.scatter(x, y, c=rho, vmin=1022.0, vmax=1042.0, cmap=cm_rho)
 ax3.tick_params(axis='x', direction='in', labelrotation=70)
 ax3.tick_params(axis='y', direction='in')
 ax3.invert_yaxis()
-ax3.set_title('$Density (kg/m^{3})$')
+ax3.set_title('Density $(kg/m^{3})$')
 ax3.set_xlabel(t_utc)
 cbar3 = fig.colorbar(sc3, ax=ax3, use_gridspec=True)
 
 # save and show figures
-fig_title = fig.suptitle('test01')  # work on how to make this a text string of Station and Node from DataGrabber.py inputs
-#fig_name = out_dir + fig_title + '.png'
-fig_name = 'test01.png'
+fig_name = fname1.replace('.p','.png')
 plt.tight_layout()
-plt.savefig(out_dir)
-plt.show()
+if save_fig:
+	plt.savefig(out_dir)
+elif not save_fig:
+	plt.show()
